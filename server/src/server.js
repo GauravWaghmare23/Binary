e82e7c70f9ffe25d9b4d633360118965758d40ab
gauravwaghmare23 <gauravwaghmare95032@gmail.com>
2026-01-01 22:09:16 +0530

    .

--- server/src/server.js
+++ server/src/server.js
@@ -1,26 +1,32 @@
-import express from "express"
+import express from "express";
 import { ENV } from "./lib/env.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://binary-frontend.onrender.com/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-    res.status(200).json({
-        success: true,
-        data: "Hello from the server.",
-    });
-})
+  res.status(200).json({
+    success: true,
+    data: "Hello from the server.",
+  });
+});
 app.get("/data", (req, res) => {
-    res.status(200).json({
-        success: true,
-        data: "This is some sample data from the server.",
-    });
+  res.status(200).json({
+    success: true,
+    data: "This is some sample data from the server.",
+  });
 });
 
-
 app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port http://localhost:${ENV.PORT}`);
-})
-
-
-
+  console.log(`Server is running on port http://localhost:${ENV.PORT}`);
+});
