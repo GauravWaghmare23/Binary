9072f8ec8d89381a59644095cfb1e4f330337d01
gauravwaghmare23 <gauravwaghmare95032@gmail.com>
2026-01-01 22:03:33 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [data, setData] = useState("")
-    try {
-      const response = await axios.get('https://binary-3gfc.onrender.com/data');
-      console.log(response.data);
-      setData(response.data.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://binary-3gfc.onrender.com/data"
+        );
+        console.log(response.data);
+        setData(response.data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h1>Welcome to Binary Project</h1>
       <h2>Data from Server: {data}</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
