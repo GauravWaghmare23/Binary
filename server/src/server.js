465142d52a73ebea08e9170eacb0d8feaaa69864
gauravwaghmare23 <gauravwaghmare95032@gmail.com>
2026-01-01 18:03:06 +0530

    build: created server and client also added packages like express,mongoose,dotenv for backend

---
+++ server/src/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Server is running"
+    })
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on port http://localhost:5000");
+})
