7cfd18a7e41e9d4bea76386f7192393bc4689d5e
gauravwaghmare23 <gauravwaghmare95032@gmail.com>
2026-01-03 01:50:54 +0530

    fix: Correct import statement for logger in stream.js

--- server/src/lib/stream.js
+++ server/src/lib/stream.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { StreamChat } from "stream-chat";
-import { logger } from './../utils/logger';
+import { logger } from './../utils/logger.js';
 
 const apiKey = process.env.STREAM_API_KEY;
 const apiSecret = process.env.STREAM_API_SECRET;
